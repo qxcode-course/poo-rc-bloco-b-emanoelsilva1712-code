1634f1d4e7a4219db8b79cff39bac814a1118fa9
emanoelsilva1712-code <emanoelsilva1712@gmail.com>
2025-10-07 15:01:45 +0000

    2 horas

---
+++ myrep/poo/chinela/py/draft.py
@@ -0,0 +1,17 @@
+class Chinela:
+    def __init__(self):
+        self.__tamanho: int = 0
+
+    def getTamanho(self):
+        return self.__tamanho
+
+    def setTamanho(self):
+        return self.__tamanho
+
+def main():
+
+main()
+
+    
+        
+

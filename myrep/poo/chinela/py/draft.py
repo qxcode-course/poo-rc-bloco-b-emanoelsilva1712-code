1657c7369644d68c28e749585a4dc153b0d51346
emanoelsilva1712-code <emanoelsilva1712@gmail.com>
2025-10-08 01:56:19 +0000

    1 hora

--- myrep/poo/chinela/py/draft.py
+++ myrep/poo/chinela/py/draft.py
@@ -5,13 +5,24 @@ class Chinela:
     def getTamanho(self):
         return self.__tamanho
 
-    def setTamanho(self):
-        return self.__tamanho
+    def setTamanho(self, novo_tamanho):
+        
+        if novo__tamanho <= 20 and novo__tamanho >= 50:
+            print("Erro, digite outro numero")
+        else:
+            print("Digite o tamanho da chinela")
+        
+        if novo__tamanho % 2 != 0:
+            print("Erro, digite outro numero")
+        else:
+            print("Digite o tamanho da chinela")
 
-def main():
+while Chinela.getTamanho() == 0:
+    print("Digite seu tamanho de chinela")
+    tamanho = int(input())
 
-main()
-
+    Chinela.setTamanho(tamanho)
+    print(f"Parabens, voce comprou uma chinela tamanh{chinela.getTamanho()}")
     
         
 

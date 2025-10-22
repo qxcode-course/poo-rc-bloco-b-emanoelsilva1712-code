e9ea10ed62345b5663dcdbe90e71065ee1decdb8
emanoelsilva1712-code <emanoelsilva1712@gmail.com>
2025-10-22 17:48:54 +0000

    grafite

---
+++ myrep/poo/grafite/py/draft.py
@@ -0,0 +1,16 @@
+class Grafite:
+    def __init__(self):
+        self.__thickness: float    #espessura do grafite
+        self.__hardness: str     #dureza
+        self.__size: int         #tamanho
+
+
+class Lapizeira:
+    def __init__(self, ponta = str):
+        self.__ponta = None
+
+
+
+    
+    
+
